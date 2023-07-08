34d6cf3d0968e8f79bed8bf7ff996a71a828f93f
Schaidt Patriota <schaidt534@gmail.com>
2023-07-07 21:27:43 -0300

    Update
    06_ParametersAndExceptionHandler

---
+++ 06_ParametersAndExceptionHandler/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -0,0 +1,30 @@
+package br.com.erudio.math;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleMath {
+    public Double sum(Double numberOne, Double numberTwo){
+        return numberOne + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo){
+        return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo){
+        return numberOne * numberTwo;
+    }
+
+    public Double division(Double numberOne, Double numberTwo){
+        return numberOne / numberTwo;
+    }
+
+    public Double mean(Double numberOne, Double numberTwo) throws Exception {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double squareRoot(Double number) throws Exception {
+        return Math.sqrt(number);
+    }
+}
