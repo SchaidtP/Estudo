ee6f6e9282e287b265624d27304f651945fcd352
Schaidt Patriota <schaidt534@gmail.com>
2023-07-15 21:03:20 -0300

    17_AuthenticationWithJWT
    Created

---
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo', 'Ayrton', 'Male', 'Senna'),
+	(2, 'São Paulo', 'Schaidt', 'Male', 'da Silva Patriota'),
+	(3, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+	(5, 'Porbandar - India', 'Mahatma', 'Male', 'Gandhi'),
+	(7, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(8, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela');
