ae94d11cbaf776005f9391ecf16891d2a7181397
Schaidt Patriota <schaidt534@gmail.com>
2023-07-16 21:38:41 -0300

    18_ShieldingOurAPIWithTests
    Finished

---
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,72 @@
+package br.com.erudio.services;
+
+import br.com.erudio.controllers.PersonController;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.exceptions.RequiredObjectIsNullException;
+import br.com.erudio.exceptions.ResourceNotFoundException;
+import br.com.erudio.mapper.ErudioMapper;
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<PersonVO> findAll() {
+        logger.info("Finding all people!");
+        var persons =  ErudioMapper.parseListObjects(personRepository.findAll(), PersonVO.class);
+        persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getId())).withSelfRel()));
+        return persons;
+    }
+
+    public PersonVO findById(Long id) {
+        logger.info("Finding one person!");
+
+        var entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        var vo = ErudioMapper.parseObject(entity, PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return vo;
+    }
+
+    public PersonVO create(PersonVO personVO) {
+        if(personVO == null) throw new RequiredObjectIsNullException();
+        logger.info("Finding one person!");
+        var entity = ErudioMapper.parseObject(personVO, Person.class);
+        var vo = ErudioMapper.parseObject(personRepository.save(entity), PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(vo.getId())).withSelfRel());
+        return vo;
+    }
+
+    public PersonVO update(PersonVO personVO) {
+        if(personVO == null) throw new RequiredObjectIsNullException();
+        logger.info("Updating one person!");
+        var entity = personRepository.findById(personVO.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+
+        entity.setFirstName(personVO.getFirstName());
+        entity.setLastName(personVO.getLastName());
+        entity.setAddress(personVO.getLastName());
+        entity.setGender(personVO.getGender());
+
+        var vo = ErudioMapper.parseObject(personRepository.save(entity), PersonVO.class);
+        vo.add(linkTo(methodOn(PersonController.class).findById(vo.getId())).withSelfRel());
+        return vo;
+    }
+
+    public void delete(Long id) {
+        logger.info("Deleting one person!");
+
+        var entity = personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        personRepository.delete(entity);
+    }
+}
---
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
+	('leandro', 'Leandro Costa', '862df5a61cefaa16602632e230e9710e867ae5786b695e958bf5911a78eefe4453db1f011a6f5493', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '68d7b19dc10f55314fa37e22c85223e6431b3238086bc648864228e86ea02fe4a4058e61cb086643', b'1', b'1', b'1', b'1');
