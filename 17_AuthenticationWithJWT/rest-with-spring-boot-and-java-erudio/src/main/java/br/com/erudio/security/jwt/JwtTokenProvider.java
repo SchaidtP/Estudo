17d0d6642fd68ec5d9b389e60364d7e5541eb411
Schaidt Patriota <schaidt534@gmail.com>
2023-07-16 21:11:52 -0300

    17_AuthenticationWithJWT
    Finished

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtTokenProvider.java
@@ -49,6 +49,17 @@ public class JwtTokenProvider {
         return new TokenVO(userName, true, now, validity, accessToken, refreshToken);
     }
 
+    public TokenVO refreshToken(String refreshToken) {
+        if (refreshToken.contains("Bearer ")) refreshToken =
+                refreshToken.substring("Bearer ".length());
+
+        JWTVerifier verifier = JWT.require(algorithm).build();
+        DecodedJWT decodedJWT = verifier.verify(refreshToken);
+        String userName = decodedJWT.getSubject();
+        List<String> roles = decodedJWT.getClaim("roles").asList(String.class);
+        return createAccessToken(userName, roles);
+    }
+
     private String getAccessToken(String userName, List<String> roles, Date now, Date validity) {
         String issuerUrl = ServletUriComponentsBuilder
                 .fromCurrentContextPath().build().toUriString();
