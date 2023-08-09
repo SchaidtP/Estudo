4e534b61e9fa3e6f2600b68e8422048cf35e1078
Schaidt Patriota <schaidt534@gmail.com>
2023-08-09 09:04:20 -0300

    Add model-spring-security

---
+++ BackEnd/SpringFramework/model-spring-security/src/main/java/br/com/springsecurity/ModelSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelSpringSecurityApplication.class, args);
+	}
+
+}
